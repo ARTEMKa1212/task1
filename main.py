36aada22f30c8c639035d632d2a3e2ad8ad374c0
Platonenko Artem <113416478+ARTEMKa1212@users.noreply.github.com>
2023-10-12 13:47:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,48 +1,38 @@
-from scipy.misc import derivative
+import numpy as np
+import matplotlib.pyplot as plt
 
+x = np.array([-4, -1, 1, 2])
+y = np.array([-6, 3, -11, -6])
+meaning = np.array([-3, -2, -0.5, 2.5])
 
+def f(x, y, meaning):
+    n = len(x)
+    m = len(meaning)
+    result = np.zeros(m)
 
-def function(x):
-    return 3 * x ** 4 - x ** 3 + 10 * x ** 2 - 5 * x - 3
+    for k in range(m):
+        p = np.zeros(n)
+        for i in range(n):
+            num = 1
+            for j in range(n):
+                if i != j:
+                    num *= (meaning[k] - x[j]) / (x[i] - x[j])
+            p[i] = num
+        result[k] = np.dot(y, p)
 
+    return result
 
-def other_function(a, b, eps):
-    first = derivative(function, a, n = 2)
-    if (function(b) * first > 0):
-        answer = b
-    else:
-        answer = a
-    second = derivative(function, answer, n = 1)
-    new_answer = answer - function(answer) / second
-    while (abs(new_answer - answer) > eps):
-        answer = new_answer
-        new_answer = answer - function(answer) / second
-    return print(new_answer)
-other_function(1,2,0.0001)
+function = f(x, y, meaning)
 
+for i in range(len(meaning)):
+    print("Значение в точке", meaning[i], ":", function[i].round(4))
 
-
-def function(x):
-    return 3 * x ** 4 - x ** 3 + 10 * x ** 2 - 5 * x - 3
-
-
-def other_function(a, b, eps):
-    if (derivative(function, a, n = 2) * (derivative(function, a, n = 1)) > 0):
-        a0 = a
-        b0 = b
-    else:
-        a0 = b
-        b0 = a
-    ai = a0
-    bi = b0
-    while (abs(ai - bi)) > eps:
-        first = ai - function(ai) * (bi - ai) / (function(bi) - function(ai))
-        second = bi - function(bi) / derivative(function, bi, n = 1)
-        ai = first
-        bi = second
-    x = (ai + bi) / 2
-
-    return print(x)
-other_function(1,2,0.0001)
-
-
+plt.figure(figsize=(8, 6))
+plt.plot(x, y, 'ro', label='Исходные точки')
+plt.plot(meaning, function, label='Интерполяция')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+plt.title('Интерполяция Лагранжа')
+plt.show()
